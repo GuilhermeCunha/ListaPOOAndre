4a09a489828cc63017e53548ba421dcc420bab47
GuilhermeCunha <guiscunha@gmail.com>
2019-04-05 10:37:43 -0300

    Final

--- 9-AtvdPOOAndre/src/pkg9/atvdpooandre/AtvdPOOAndre.java
+++ 9-AtvdPOOAndre/src/pkg9/atvdpooandre/AtvdPOOAndre.java
@@ -15,8 +15,20 @@ public class AtvdPOOAndre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Guilherme" , 18, 1.80, 71, "Masculino");
-        System.out.println(pessoa);
+        Pessoa[] pessoas = new Pessoa[4];
+        
+        pessoas[0] = new Pessoa("Guilherme" , 18, 1.80, 71, "Masculino");
+        System.out.println(pessoas[0]);
+        
+        pessoas[1] = new Pessoa("Gabriel" , 18, 2.0, 50, "Masculino");
+        System.out.println(pessoas[1]);  
+        
+        pessoas[2] = new Pessoa("Luca" , 18, 1.50, 71, "Masculino");
+        System.out.println(pessoas[2]); 
+        
+        pessoas[3] = new Pessoa("Mehiel" , 18, 1.80, 85, "Masculino");
+        System.out.println(pessoas[3]);
+         
     }
     
 }
